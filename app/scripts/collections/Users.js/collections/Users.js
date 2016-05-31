178f795791099f7d555a419c13cbb8788ba87929
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-06-01 08:08:25 +0900

    collectionとmodelを作った

---
+++ app/scripts/collections/Users.js/collections/Users.js
@@ -0,0 +1,8 @@
+var Backbone = require('backbone');
+Backbone.LocalStorage = require('backbone.localstorage');
+var User = require('../models/User');
+
+module.exports = Backbone.Collection.extend({
+    model: User,
+    localStorage: new Backbone.LocalStorage('BackboneMarionetteTemplate.Users')
+});

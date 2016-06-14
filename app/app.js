5a944fe6a7d9d50e580a69bc3d1cdb25162c7431
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-06-14 16:24:53 +0900

    サイドバーのリンクのしかたを変更

--- app/app.js
+++ app/app.js
@@ -279,41 +279,7 @@ Backbone.Marionette = require('backbone.marionette');
 module.exports = Backbone.Marionette.ItemView.extend({
     tagName: 'ul',
     className: 'nav nav-pills nav-stacked',
-    template: '#side_menu_view',
-    ui: {
-        requests: '.requests',
-        newRequest: '.new-request',
-        users: '.users',
-        categories: '.categories',
-        statusList: '.status-list'
-    },
-    events: {
-        'click @ui.requests': 'onClickRequestsLink',
-        'click @ui.newRequest': 'onClickNewRequestLink',
-        'click @ui.users': 'onClickUsersLink',
-        'click @ui.categories': 'onClickCategoriesLink',
-        'click @ui.statusList': 'onClickStatusListLink'
-    },
-    onClickRequestsLink: function(e) {
-        e.preventDefault();
-        Backbone.history.navigate('requests', {trigger: true});
-    },
-    onClickNewRequestLink: function(e) {
-        e.preventDefault();
-        Backbone.history.navigate('requests/new', {trigger: true});
-    },
-    onClickUsersLink: function(e) {
-        e.preventDefault();
-        Backbone.history.navigate('users', {trigger: true});
-    },
-    onClickCategoriesLink: function(e) {
-        e.preventDefault();
-        Backbone.history.navigate('categories', {trigger: true});
-    },
-    onClickStatusListLink: function(e) {
-        e.preventDefault();
-        Backbone.history.navigate('status_list', {trigger: true});
-    }
+    template: '#side_menu_view'
 });
 
 },{"backbone":"backbone","backbone.marionette":31}],12:[function(require,module,exports){

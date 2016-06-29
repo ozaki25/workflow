f6daad6584af42a0b24cc438c2daa34cba0d8de5
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-06-29 12:03:29 +0900

    １つの申請で複数ファイルをアップロードできるようにした

--- app/models/Document.js
+++ app/models/Document.js
@@ -1,7 +1,4 @@
 var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
-    setUrl: function(requestId) {
-        this.url = 'http://localhost:8080/requests/' + requestId + '/documents'
-    }
 });
--- app/views/requests/FormView.js
+++ app/views/requests/FormView.js
@@ -1,10 +1,10 @@
 var $ = require('jquery');
+var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.Marionette = require('backbone.marionette');
 Backbone.Validation = require('backbone.validation');
 var Request = require('../../models/Request');
 var Status = require('../../models/Status');
-var Document = require('../../models/Document');
 var Documents = require('../../collections/Documents');
 
 module.exports = Backbone.Marionette.ItemView.extend({
@@ -12,10 +12,13 @@ module.exports = Backbone.Marionette.ItemView.extend({
     template: '#request_form_view',
     ui: {
         requestForm: '#request_form',
+        inputFile: 'input.file-tmp',
         saveBtn: '.save-btn',
         submitBtn: '.submit-btn'
     },
     events: {
+        'change @ui.inputFile': 'selectedFile',
+        'click .remove-file': 'onClickRemoveFile',
         'click @ui.saveBtn': 'onClickSave',
         'click @ui.submitBtn': 'onClickSubmit'
     },
@@ -49,6 +52,28 @@ module.exports = Backbone.Marionette.ItemView.extend({
             inputedContent: this.model.isNew() ? '' : this.model.get('content')
         }
     },
+    selectedFile: function(e) {
+        var input = this.$(e.target);
+        var file = input.prop('files');
+        if(file.length !== 0) {
+            var latestFileNo = this.$('input.file:last').attr('id');
+            var fileNo = latestFileNo ? parseInt(latestFileNo) + 1 : 1
+            var newInput = input.clone();
+            input.after(newInput);
+            input.after(
+                '<p class="' + fileNo + '">' +
+                    '<span>' + file[0].name + '</span>' +
+                    '<a href="#" class="btn btn-link btn-xs remove-file">&times;</a>' +
+                '</p>'
+            );
+            input.attr('id', fileNo).addClass('file hide ' + fileNo).removeClass('file-tmp');
+        }
+    },
+    onClickRemoveFile: function(e) {
+        e.preventDefault();
+        var fileNo = $(e.target).closest('p').attr('class');
+        this.$('.' + fileNo).remove();
+    },
     onClickSave: function() {
         this.saveRequest(1, false);
     },
@@ -70,26 +95,29 @@ module.exports = Backbone.Marionette.ItemView.extend({
         var options = {
             wait: true,
             success: function(request) {
-                var file = formData.get('file');
-                if(file.size === 0) {
-                    Backbone.history.navigate('/requests', {trigger: true});
-                } else {
-                    var document = new Document();
-                    document.setUrl(request.id);
-                    var fileData = new FormData();
-                    fileData.append('file', file);
-                    var options = {
-                        processData: false,
-                        contentType: false,
-                        data: fileData,
-                        wait: true,
-                        success: function() {
-                            Backbone.history.navigate('/requests', {trigger: true})
+                var inputFiles = this.$('input.file');
+                console.log('File count : ', inputFiles.length);
+                if(inputFiles.length) {
+                    _(inputFiles).each(function(file) {
+                        var fileData = new FormData();
+                        fileData.append('file', this.$(file).prop('files')[0]);
+                        var options = {
+                            processData: false,
+                            contentType: false,
+                            data: fileData,
+                            wait: true,
+                            success: function() {
+                                Backbone.history.navigate('/requests', {trigger: true})
+                            }
                         }
-                    }
-                    document.save({}, options);
+                        var documents = new Documents();
+                        documents.setUrl(request.id);
+                        documents.create({}, options);
+                    }.bind(this))
+                } else {
+                    Backbone.history.navigate('/requests', {trigger: true});
                 }
-            }
+            }.bind(this)
         };
         this.model.save({}, options);
     },

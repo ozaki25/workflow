ffc73156f58a0a14c15e1a8f9180ed0d4eefc091
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-07-01 18:55:58 +0900

    入力しやすいようにUIDをみじかくした

--- app/collections/Users.js
+++ app/collections/Users.js
@@ -5,11 +5,11 @@ module.exports = Backbone.Collection.extend({
     model: User,
     url: '/users',
     addDefaultUser: function() {
-        this.createUser('ABC0001', 'アドミンユーザ', '管理チーム', 0, true);
-        this.createUser('ABC0002', '一般ユーザ', '開発チーム', 3, false);
-        this.createUser('ABC0003', '役席ユーザ', '開発チーム', 2, false);
-        this.createUser('ABC0004', '受付ユーザ', '運用チーム', 2, true);
-        this.createUser('ABC0005', '担当ユーザ', '運用チーム', 4, false);
+        this.createUser('001', 'アドミンユーザ', '管理チーム', 0, true);
+        this.createUser('002', '一般ユーザ', '開発チーム', 3, false);
+        this.createUser('003', '役席ユーザ', '開発チーム', 2, false);
+        this.createUser('004', '受付ユーザ', '運用チーム', 2, true);
+        this.createUser('005', '担当ユーザ', '運用チーム', 4, false);
     },
     createUser: function(uid, name, team, jobLevel, admin) {
         this.create({

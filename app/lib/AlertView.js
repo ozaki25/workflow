d3d65066bccb96a96ca9457e40e95c25c67b5f53
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-09-23 09:52:02 +0900

    部品を最新化

---
+++ app/lib/AlertView.js
@@ -0,0 +1,52 @@
+var _ = require('underscore');
+var Backbone = require('backbone');
+Backbone.Marionette = require('backbone.marionette');
+
+module.exports = Backbone.Marionette.LayoutView.extend({
+    className: function() {
+        var classNames = (this.options._className || '') + ' alert alert-dismissible ';
+        switch(this.options.alertType) {
+            case 'success':
+                classNames += 'alert-success';
+                break;
+            case 'info':
+                classNames += 'alert-info';
+                break;
+            case 'warning':
+                classNames += 'alert-warning';
+                break;
+            case 'danger':
+                classNames += 'alert-danger';
+                break;
+            default:
+                classNames += 'alert-success';
+                break;
+        }
+        return classNames;
+    },
+    attributes: function() {
+        return Backbone.$.extend(this.options.attrs, {
+            id: this.options._id,
+        });
+    },
+    template: _.template(
+        '<button type="button" class="close" data-dismiss="alert">' +
+          '<span aria-hidden="true">&times;</span>' +
+        '</button>' +
+        '<%= message %>'
+    ),
+    templateHelpers: function() {
+        return {
+            message: typeof this.message === 'string' ? this.message : '<div id="message_region"></div>',
+        }
+    },
+    initialize: function(options) {
+        this.message = options.message || '';
+    },
+    onBeforeShow: function() {
+        if(typeof this.message === 'object') {
+            this.addRegion('messageRegion', '#message_region');
+            this.getRegion('messageRegion').show(this.message);
+        }
+    },
+});
--- app/lib/GridView.js
+++ app/lib/GridView.js
@@ -1,7 +1,6 @@
 var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.Marionette = require('backbone.marionette');
-var ButtonView = require('./ButtonView');
 
 var GridRowView = Backbone.Marionette.LayoutView.extend({
     tagName: 'tr',
@@ -21,11 +20,15 @@ var GridRowView = Backbone.Marionette.LayoutView.extend({
             }.bind(this))
         }
     },
+    events: {
+        'click': 'onClick',
+    },
     initialize: function(options) {
         this.columns = options.columns;
         _(this.columns).map(function(col) {
             if(col.child) col.view = new col.child.view(col.child.options);
         });
+        this.clickRowEventName = options.clickRowEventName;
         this.eventNames = options.eventNames;
         this.addChildEvents();
     },
@@ -36,6 +39,10 @@ var GridRowView = Backbone.Marionette.LayoutView.extend({
                 this.getRegion(this.model.id + col.view.cid).show(col.view);
             }
         }.bind(this));
+    },
+    onClick: function(e) {
+        e.preventDefault();
+        this.triggerMethod(this.clickRowEventName, e);
     },
     addChildEvents: function() {
         _(this.eventNames).each(function(eventName) {
@@ -61,6 +68,7 @@ var GridView = Backbone.Marionette.CompositeView.extend({
     childViewOptions: function() {
         return {
             columns: this.columns,
+            clickRowEventName: this.clickRowEventName,
             eventNames: this.eventNames,
         }
     },
@@ -80,7 +88,8 @@ var GridView = Backbone.Marionette.CompositeView.extend({
     initialize: function(options) {
         this.sortable = options.sort;
         this.columns = options.columns;
-        this.eventNames = options.eventNames
+        this.clickRowEventName = options.clickRowEventName || 'click:row';
+        this.eventNames = options.eventNames;
     },
     ui: {
          tableHeader: 'th.table-header',

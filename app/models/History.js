35db6800b857a72117d595062a3c2b31b4339e35
ozaki25 <fs65ugumori@yahoo.co.jp>
2016-09-26 13:21:06 +0900

    日付操作用にmomentjsを入れた

--- app/models/History.js
+++ app/models/History.js
@@ -1,8 +1,9 @@
+var moment = require('moment');
 var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
     parse: function(data) {
-        data.createdDate = new Date(data.createdDate).toLocaleDateString("ja-JP");
+        data.createdDate = moment(new Date(data.createdDate)).format('YYYY/MM/DD HH:mm');
         return data;
     }
 });
